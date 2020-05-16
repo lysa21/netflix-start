eab241314fc918e81a30f83db2b3ee0281ade1b8
lysa <lysafichot@gmail.com>
2020-05-16 10:28:10 +0200

    Config compilation sass to css with Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,34 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
 module.exports = {
-    entry: './index.js',
-    module: {
-      rules: [
-        {
-          test: /\.(js)$/,
-          exclude: /node_modules/,
-          use: ['babel-loader']
-        }
-      ]
-    },
-    resolve: {
-      extensions: ['*', '.js']
-    },
-    output: {
-      path: __dirname + '/build',
-      publicPath: '/',
-      filename: '[name].bundle.js'
-    }
-  };
+  entry: ["./index.js", "./sass/style.scss"],
+  output: {
+    path: __dirname + "/build",
+    publicPath: "/",
+    filename: "[name].bundle.js",
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/[name].css",
+      chunkFilename: "[id].css",
+    }),
+    //new ExtractTextPlugin("./css/[name].css"),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.scss$/,
+        use: [{ loader: MiniCssExtractPlugin.loader }, { loader: "css-loader" }, { loader: "sass-loader" }],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js"],
+  },
+};
